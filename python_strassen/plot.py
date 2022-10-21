59e5ecd34dc7f411ba5743336fd2258653b3faa6
Steboss89 <stbo@trustpilot.com>
2022-10-21 11:28:20 +0100

    strassen in python

---
+++ python_strassen/plot.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt 
+import matplotlib
+import seaborn as sbn 
+import pandas as pd 
+
+sbn.set_style("whitegrid")
+palette = sbn.color_palette() 
+matplotlib.rc('xtick', labelsize=20)
+matplotlib.rc('ytick', labelsize=20)
+
+df = pd.read_csv("python_results.csv")
+x_axis = df['SIZE']
+base_y = df[' BASE_RES_MEAN']
+base_err = df[' BASE_RES_STD']
+
+opt_y = df[' OPT_RES_MEAN']
+opt_err = df[' OPT_RES_STD']
+
+fig,ax = plt.subplots(figsize=[15,10])
+ax.set_xlabel("Matrix Size", fontsize=20)
+ax.set_ylabel("Time / s", fontsize=20)
+ax.errorbar(x_axis, base_y, yerr=base_err, c=palette[0], label="MatMul")
+ax.errorbar(x_axis, opt_y, yerr=opt_err, c=palette[1], label="Strassen")
+ax.legend(loc="best", fontsize=20)
+fig.savefig("strassen.png", dpi=300)
---
+++ python_strassen/strassen.py
@@ -0,0 +1,124 @@
+# This script is calibrated for square matrices of integers
+import numpy as np
+import time
+
+
+SIZES = [128, 256, 512, 768, 1024, 1280, 2048, ]
+THRESHOLD = 8
+
+
+def strassen(A, B):
+    r""" Strassen is applied recursively. 
+    As a constraint we are stopping the iteration when 
+    the submatrix has riched a size of THRESHOLD
+
+    Parameters
+    ----------
+    A: np.array: block matrix A
+    B: np.array: block matrix B
+
+    Return 
+    ------
+    C: np.array: product matrix C
+    """
+    current_size = len(A) # we are taking just matrix A, as B has the same size
+    if current_size <= THRESHOLD:
+        C = np.matmul(A, B) 
+        return C 
+    else:
+        current_new_size = current_size//2 
+        # now extract all the blocks from the input matrix of size current_new_size 
+        a11 = A[0:current_new_size,0:current_new_size] # top left
+        a12 = A[0:current_new_size,current_new_size:current_size] # top right
+        a21 = A[current_new_size:current_size, 0:current_new_size] # bottom left
+        a22 = A[current_new_size:current_size, current_new_size:current_size] # bottom right
+        # same fo rB 
+        b11 = B[0:current_new_size,0:current_new_size] # top left
+        b12 = B[0:current_new_size,current_new_size:current_size] # top right
+        b21 = B[current_new_size:current_size, 0:current_new_size] # bottom left
+        b22 = B[current_new_size:current_size, current_new_size:current_size] # bottom right
+        # roll over Strassen
+        a_ = np.add(a11, a22)
+        b_ = np.add(b11, b22) 
+        prod1 = strassen(a_, b_) # iterate over the first multiplication
+
+        a_ = np.add(a21, a22) 
+        prod2 = strassen(a_, b11) # second product 
+
+        b_ = np.subtract(b12, b22) 
+        prod3 = strassen(a11, b_) # third product 
+
+        b_ = np.subtract(b21, b11)
+        prod4 = strassen(a22, b_) # fourth product 
+
+        a_ = np.add(a11, a12) 
+        prod5 = strassen(a_, b22) # fifth product 
+
+        a_ = np.subtract(a21, a11) 
+        b_ = np.add(b11, b12) 
+        prod6 = strassen(a_, b_) # sixth product 
+
+        a_ = np.subtract(a12, a22) 
+        b_ = np.add(b21, b22) 
+        prod7 = strassen(a_, b_) # seventh product
+
+        # compute the c element for the product matrix 
+        c12 = np.add(prod3, prod5) 
+        c21 = np.add(prod2, prod4) 
+        
+        a_ = np.add(prod1, prod4)
+        b_ = np.add(a_, prod7)
+        c11 = np.subtract(b_, prod5)
+
+        a_ = np.add(prod1, prod3) 
+        b_ = np.add(a_, prod6) 
+        c22 = np.subtract(b_, prod2)
+
+        # return the final matrix 
+        C = np.zeros([current_size, current_size])
+        C[0:current_new_size, 0:current_new_size] = c11 # top left
+        C[0:current_new_size,current_new_size:current_size] = c12 # top right
+        C[current_new_size:current_size, 0:current_new_size] = c21 # bottom left
+        C[current_new_size:current_size, current_new_size:current_size] = c22 # bottom right
+
+        return C
+
+# fix the random generator 
+np.random.seed(42)
+# definition of lists for plots 
+results = {}
+# and save on a file 
+ofile = open("python_results.csv", "w")
+ofile.write("SIZE, BASE_RES_MEAN, BASE_RES_STD, OPT_RES_MEAN, OPT_RES_STD\n")
+for SIZE in SIZES: 
+    # generate INTEGERS [1-9] random matrices 
+    A = np.random.randint(low=1, high=9, size=(SIZE, SIZE))
+    B = np.random.randint(low=1, high=9, size=(SIZE,SIZE))
+    # list to store results
+    base_res = [] 
+    strass_res = []
+    # run at least 3 trials for each matrix
+    for trial in range(0, 3):
+        # numpy multiplication 
+        start = time.time()
+        C = np.matmul(A, B)
+        end = time.time()
+        total_time = end - start
+        print(f"total time {total_time}s")
+        #print(C[0])
+        base_res.append(total_time)
+        # strassen 
+        start = time.time() 
+        C = strassen(A, B)
+        end = time.time() 
+        total_time = end - start
+        print(f"total time {total_time}")
+        strass_res.append(total_time)
+        #print(C[0])
+    # compute the average and the stddev 
+    results[SIZE] = [np.mean(base_res), np.std(base_res),\
+                     np.mean(strass_res), np.std(strass_res)]
+    # for safety save in a file 
+    ofile.write(f"{SIZE},{np.mean(base_res)}, {np.std(base_res)}, {np.mean(strass_res)}, {np.std(strass_res)}\n")
+ofile.close()
+# plot
